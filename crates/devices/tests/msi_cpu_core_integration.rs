7297d26028671479e710c0c240982c639722f9f7
root <root@ip-10-140-21-115.ec2.internal>
2026-01-10 12:13:08 +0000

    test: exercise MSI delivery through aero_cpu_core
    - Add integration test that programs MSI in PCI config space, triggers it via PlatformInterrupts, and verifies aero_cpu_core delivers the expected IDT vector\n- Add aero_cpu_core as a dev-dependency for aero-devices tests\n- Fix duplicated lru entries in Cargo.lock (lockfile was unparsable)

---
+++ crates/devices/tests/msi_cpu_core_integration.rs
@@ -0,0 +1,87 @@
+use aero_cpu_core::bus::{Bus, RamBus};
+use aero_cpu_core::interrupts::{CpuExit, InterruptController as CpuInterruptController};
+use aero_cpu_core::system::{Cpu, CpuMode, DescriptorTableRegister};
+use aero_devices::pci::{MsiCapability, PciConfigSpace};
+use aero_platform::interrupts::{InterruptController, PlatformInterruptMode, PlatformInterrupts};
+
+fn write_idt_gate32(
+    mem: &mut RamBus,
+    idt_base: u64,
+    vector: u8,
+    selector: u16,
+    offset: u32,
+    type_attr: u8,
+) {
+    let entry_addr = idt_base + (vector as u64) * 8;
+    mem.write_u16(entry_addr, (offset & 0xffff) as u16);
+    mem.write_u16(entry_addr + 2, selector);
+    mem.write_u8(entry_addr + 4, 0);
+    mem.write_u8(entry_addr + 5, type_attr);
+    mem.write_u16(entry_addr + 6, (offset >> 16) as u16);
+}
+
+struct PlatformCtrl<'a> {
+    ints: &'a mut PlatformInterrupts,
+    last_vector: Option<u8>,
+}
+
+impl CpuInterruptController for PlatformCtrl<'_> {
+    fn poll_interrupt(&mut self) -> Option<u8> {
+        let vector = self.ints.get_pending()?;
+        self.ints.acknowledge(vector);
+        self.last_vector = Some(vector);
+        Some(vector)
+    }
+}
+
+#[test]
+fn msi_interrupt_delivers_to_cpu_core_idt() -> Result<(), CpuExit> {
+    let mut config = PciConfigSpace::new(0x1234, 0x5678);
+    config.add_capability(Box::new(MsiCapability::new()));
+    let cap_offset = config
+        .find_capability(aero_devices::pci::msi::PCI_CAP_ID_MSI)
+        .unwrap() as u16;
+
+    config.write(cap_offset + 0x04, 4, 0xfee0_0000);
+    config.write(cap_offset + 0x08, 4, 0);
+    config.write(cap_offset + 0x0c, 2, 0x0045);
+    let ctrl = config.read(cap_offset + 0x02, 2) as u16;
+    config.write(cap_offset + 0x02, 2, (ctrl | 0x0001) as u32);
+
+    let mut ints = PlatformInterrupts::new();
+    ints.set_mode(PlatformInterruptMode::Apic);
+
+    let msi = config.capability_mut::<MsiCapability>().unwrap();
+    assert!(msi.trigger(&mut ints));
+
+    let mut mem = RamBus::new(0x20000);
+    let idt_base = 0x1000;
+    let handler = 0x6000;
+    write_idt_gate32(&mut mem, idt_base, 0x45, 0x08, handler, 0x8e);
+
+    let mut cpu = Cpu::default();
+    cpu.mode = CpuMode::Protected32;
+    cpu.idtr = DescriptorTableRegister {
+        base: idt_base,
+        limit: 0x7ff,
+    };
+    cpu.cs = 0x08;
+    cpu.ss = 0x10;
+    cpu.rsp = 0x9000;
+    cpu.rip = 0x1111;
+    cpu.rflags = 0x202;
+
+    let mut ctrl = PlatformCtrl {
+        ints: &mut ints,
+        last_vector: None,
+    };
+    cpu.poll_and_deliver_external_interrupt(&mut mem, &mut ctrl)?;
+    assert_eq!(cpu.rip, handler as u64);
+
+    let vector = ctrl
+        .last_vector
+        .expect("expected MSI vector to be acknowledged");
+    ctrl.ints.eoi(vector);
+
+    Ok(())
+}
