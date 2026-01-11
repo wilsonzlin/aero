2d08c30f5386df2214ffcefea92c68615f50c0d0
root <root@ip-10-140-20-78.ec2.internal>
2026-01-11 05:42:46 -0800

    test(aero-d3d11): import VecGuestMemory from aero_gpu::guest_memory

--- crates/aero-d3d11/tests/aerogpu_cmd_bindings.rs
+++ crates/aero-d3d11/tests/aerogpu_cmd_bindings.rs
@@ -1,6 +1,6 @@
 use aero_d3d11::input_layout::fnv1a_32;
 use aero_d3d11::runtime::aerogpu_cmd_executor::AerogpuD3d11Executor;
-use aero_gpu::VecGuestMemory;
+use aero_gpu::guest_memory::VecGuestMemory;
 use aero_protocol::aerogpu::aerogpu_cmd::{
     AerogpuCmdHdr as ProtocolCmdHdr, AerogpuCmdOpcode,
     AerogpuCmdStreamHeader as ProtocolCmdStreamHeader, AerogpuPrimitiveTopology,
