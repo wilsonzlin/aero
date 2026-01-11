7e2c7f5793d11a510ac1814519f61d38214b6533
root <root@ip-10-140-20-78.ec2.internal>
2026-01-11 14:55:02 -0800

    chore(win7_dbgctl): fix feature output formatting indentation

--- drivers/aerogpu/tools/win7_dbgctl/src/aerogpu_dbgctl.cpp
+++ drivers/aerogpu/tools/win7_dbgctl/src/aerogpu_dbgctl.cpp
@@ -843,7 +843,7 @@ static int DoQueryVersion(const D3DKMT_FUNCS *f, D3DKMT_HANDLE hAdapter) {
     if (!any) {
       wprintf(L" (none)");
     }
-  wprintf(L"\n");
+    wprintf(L"\n");
   }
 
   DumpFenceSnapshot();
