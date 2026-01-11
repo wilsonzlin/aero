27a339b33447404026d6792b4fc139487dcffd90
root <root@ip-10-140-20-78.ec2.internal>
2026-01-10 17:48:28 -0800

    chore(web): include WebHID typings
    Add @types/w3c-web-hid and reference it from web/src/vite-env.d.ts so WebHID contract/normalization code can use HIDDevice.collections, HIDCollectionInfo, HIDReportInfo, and HIDReportItem across TS versions.

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="@webgpu/types" />
 /// <reference types="wicg-file-system-access" />
+/// <reference types="w3c-web-hid" />
 
 export {};
 
