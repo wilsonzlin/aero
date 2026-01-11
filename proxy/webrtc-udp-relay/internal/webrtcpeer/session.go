a8c745dfba95277c61ec97ce21c5a5442181d199
root <root@ip-10-140-20-78.ec2.internal>
2026-01-10 23:31:32 -0800

    chore(webrtc-udp-relay): log/describe unordered l2 channel rejection
    - Distinguish unordered l2 DataChannel rejection reason in server logs\n- Document that the relay closes unordered l2 channels\n- Fix config tests under default AUTH_MODE=api_key

--- proxy/webrtc-udp-relay/internal/webrtcpeer/session.go
+++ proxy/webrtc-udp-relay/internal/webrtcpeer/session.go
@@ -87,6 +87,8 @@ func NewSession(api *webrtc.API, iceServers []webrtc.ICEServer, relayCfg relay.C
 				maxPacketLifeTime := dc.MaxPacketLifeTime()
 				if maxRetransmits != nil || maxPacketLifeTime != nil {
 					reason = "partial_reliability"
+				} else if !dc.Ordered() {
+					reason = "unordered"
 				}
 
 				var maxRetransmitsValue any
