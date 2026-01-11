69fd8f753565f4df9897780df15470736bcf65cf
root <root@ip-10-140-20-78.ec2.internal>
2026-01-11 04:46:02 -0800

    test(win7): compile d3d11_texture shaders as SM4 on FL10

--- drivers/aerogpu/tests/win7/d3d11_texture/main.cpp
+++ drivers/aerogpu/tests/win7/d3d11_texture/main.cpp
@@ -333,11 +333,13 @@ static int RunD3D11Texture(int argc, char** argv) {
   std::vector<unsigned char> vs_bytes;
   std::vector<unsigned char> ps_bytes;
   std::string shader_err;
+  const char* vs_profile = (chosen_level >= D3D_FEATURE_LEVEL_10_0) ? "vs_4_0" : "vs_4_0_level_9_1";
+  const char* ps_profile = (chosen_level >= D3D_FEATURE_LEVEL_10_0) ? "ps_4_0" : "ps_4_0_level_9_1";
   if (!aerogpu_test::CompileHlslToBytecode(kTextureHlsl,
                                            strlen(kTextureHlsl),
                                            "d3d11_texture.hlsl",
                                            "vs_main",
-                                           "vs_4_0_level_9_1",
+                                           vs_profile,
                                            &vs_bytes,
                                            &shader_err)) {
     return reporter.Fail("failed to compile vertex shader: %s", shader_err.c_str());
@@ -346,7 +348,7 @@ static int RunD3D11Texture(int argc, char** argv) {
                                            strlen(kTextureHlsl),
                                            "d3d11_texture.hlsl",
                                            "ps_main",
-                                           "ps_4_0_level_9_1",
+                                           ps_profile,
                                            &ps_bytes,
                                            &shader_err)) {
     return reporter.Fail("failed to compile pixel shader: %s", shader_err.c_str());
