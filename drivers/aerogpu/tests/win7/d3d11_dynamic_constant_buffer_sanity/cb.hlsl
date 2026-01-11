a0d98b9b650abe5a6eb5c8e56227532c505ab587
root <root@ip-10-140-20-78.ec2.internal>
2026-01-11 02:45:25 -0800

    tests(win7): sync dynamic CB HLSL source
    - Keep d3d11_dynamic_constant_buffer_sanity/cb.hlsl consistent with embedded shader

--- drivers/aerogpu/tests/win7/d3d11_dynamic_constant_buffer_sanity/cb.hlsl
+++ drivers/aerogpu/tests/win7/d3d11_dynamic_constant_buffer_sanity/cb.hlsl
@@ -1,5 +1,6 @@
 cbuffer Cb0 : register(b0) {
-  float4 color;
+  float4 vs_color;
+  float4 ps_mul;
 };
 
 struct VSIn {
@@ -8,15 +9,16 @@ struct VSIn {
 
 struct VSOut {
   float4 pos : SV_Position;
+  float4 color : COLOR0;
 };
 
 VSOut vs_main(VSIn input) {
   VSOut o;
   o.pos = float4(input.pos.xy, 0.0f, 1.0f);
+  o.color = vs_color;
   return o;
 }
 
 float4 ps_main(VSOut input) : SV_Target {
-  return color;
+  return input.color * ps_mul;
 }
-
