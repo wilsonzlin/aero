fd889ba5c1b7494617dcb01cc6cbc6d3d6d39dc3
root <root@ip-10-140-20-78.ec2.internal>
2026-01-10 23:06:53 -0800

    fix(web): remove duplicate ordered field in L2 relay signaling test

--- web/src/net/l2RelaySignalingClient.test.ts
+++ web/src/net/l2RelaySignalingClient.test.ts
@@ -7,7 +7,6 @@ type Listener = (evt: Event) => void;
 class FakeRtcDataChannel {
   label = "";
   readyState: RTCDataChannelState;
-  ordered = true;
   maxRetransmits: number | null = null;
   maxPacketLifeTime: number | null = null;
 
