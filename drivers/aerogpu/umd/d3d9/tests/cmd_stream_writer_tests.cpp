a5249c8e3f01fa2e24e6666246bfdde27a4f95c9
Wilson Lin <code@wilsonl.in>
2026-01-13 15:33:24 -0800

    test(aerogpu-d3d9): verify Patch + ProcessVertices device funcs wired

--- drivers/aerogpu/umd/d3d9/tests/cmd_stream_writer_tests.cpp
+++ drivers/aerogpu/umd/d3d9/tests/cmd_stream_writer_tests.cpp
@@ -961,6 +961,82 @@ bool TestEventQueryGetDataSemantics() {
   return true;
 }
 
+bool TestDeviceFuncsIncludePatchAndProcessVertices() {
+  struct Cleanup {
+    D3D9DDI_ADAPTERFUNCS adapter_funcs{};
+    D3D9DDI_DEVICEFUNCS device_funcs{};
+    D3DDDI_HADAPTER hAdapter{};
+    D3DDDI_HDEVICE hDevice{};
+    bool has_adapter = false;
+    bool has_device = false;
+
+    ~Cleanup() {
+      if (has_device && device_funcs.pfnDestroyDevice) {
+        device_funcs.pfnDestroyDevice(hDevice);
+      }
+      if (has_adapter && adapter_funcs.pfnCloseAdapter) {
+        adapter_funcs.pfnCloseAdapter(hAdapter);
+      }
+    }
+  } cleanup;
+
+  D3DDDIARG_OPENADAPTER2 open{};
+  open.Interface = 1;
+  open.Version = 1;
+  D3DDDI_ADAPTERCALLBACKS callbacks{};
+  D3DDDI_ADAPTERCALLBACKS2 callbacks2{};
+  open.pAdapterCallbacks = &callbacks;
+  open.pAdapterCallbacks2 = &callbacks2;
+  open.pAdapterFuncs = &cleanup.adapter_funcs;
+
+  HRESULT hr = ::OpenAdapter2(&open);
+  if (!Check(hr == S_OK, "OpenAdapter2")) {
+    return false;
+  }
+  if (!Check(open.hAdapter.pDrvPrivate != nullptr, "OpenAdapter2 returned adapter handle")) {
+    return false;
+  }
+  cleanup.hAdapter = open.hAdapter;
+  cleanup.has_adapter = true;
+
+  if (!Check(cleanup.adapter_funcs.pfnCreateDevice != nullptr, "CreateDevice entrypoint must be available")) {
+    return false;
+  }
+
+  D3D9DDIARG_CREATEDEVICE create_dev{};
+  create_dev.hAdapter = open.hAdapter;
+  create_dev.Flags = 0;
+
+  hr = cleanup.adapter_funcs.pfnCreateDevice(&create_dev, &cleanup.device_funcs);
+  if (!Check(hr == S_OK, "CreateDevice")) {
+    return false;
+  }
+  if (!Check(create_dev.hDevice.pDrvPrivate != nullptr, "CreateDevice returned device handle")) {
+    return false;
+  }
+  cleanup.hDevice = create_dev.hDevice;
+  cleanup.has_device = true;
+
+  if (!Check(cleanup.device_funcs.pfnDrawRectPatch != nullptr,
+             "D3D9DDI_DEVICEFUNCS.pfnDrawRectPatch must be non-null")) {
+    return false;
+  }
+  if (!Check(cleanup.device_funcs.pfnDrawTriPatch != nullptr,
+             "D3D9DDI_DEVICEFUNCS.pfnDrawTriPatch must be non-null")) {
+    return false;
+  }
+  if (!Check(cleanup.device_funcs.pfnDeletePatch != nullptr,
+             "D3D9DDI_DEVICEFUNCS.pfnDeletePatch must be non-null")) {
+    return false;
+  }
+  if (!Check(cleanup.device_funcs.pfnProcessVertices != nullptr,
+             "D3D9DDI_DEVICEFUNCS.pfnProcessVertices must be non-null")) {
+    return false;
+  }
+
+  return true;
+}
+
 bool TestAdapterCapsAndQueryAdapterInfo() {
   struct Cleanup {
     D3D9DDI_ADAPTERFUNCS adapter_funcs{};
@@ -13473,6 +13549,7 @@ int main() {
   failures += !aerogpu::TestFixedPacketPadding();
   failures += !aerogpu::TestOwnedAndBorrowedStreamsMatch();
   failures += !aerogpu::TestEventQueryGetDataSemantics();
+  failures += !aerogpu::TestDeviceFuncsIncludePatchAndProcessVertices();
   failures += !aerogpu::TestAdapterCapsAndQueryAdapterInfo();
   failures += !aerogpu::TestAdapterMultisampleQualityLevels();
   failures += !aerogpu::TestAdapterCachingUpdatesCallbacks();
