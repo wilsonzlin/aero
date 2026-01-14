0e095571b32548e5fd7d0e8dfb03b23dbf43dd60
Wilson Lin <code@wilsonl.in>
2026-01-14 12:26:27 -0800

    test(xtask): assert help mentions node-dir aliases for test-all and web

---
+++ xtask/tests/web_cli.rs
@@ -0,0 +1,18 @@
+#![cfg(not(target_arch = "wasm32"))]
+
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+#[test]
+fn web_help_mentions_node_dir_aliases() {
+    Command::new(env!("CARGO_BIN_EXE_xtask"))
+        .args(["web", "--help"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("--node-dir"))
+        .stdout(predicate::str::contains("--web-dir"))
+        .stdout(predicate::str::contains("AERO_NODE_DIR"))
+        .stdout(predicate::str::contains("AERO_WEB_DIR"))
+        .stdout(predicate::str::contains("WEB_DIR"));
+}
+
